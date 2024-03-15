e3853b3a66b97421e8598ec74b583db12f5003c1
Mikhail Sulim <mika-1984@yandex.ru>
2024-03-15 14:33:42 +0300

    fix: update readme, fix deploy error

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,11 +3,20 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: 'Todo App',
+    appIcon: './public/favicon.ico',
+    meta: {
+      description: 'Todo application',
+    },
+  },
   output: {
+    assetPrefix: 'https://mikhailsulim.github.io/test_chatapp/',
     distPath: {
       root: 'build',
       js: 'static',
       css: 'static',
+      image: 'static',
     },
   },
 });
